61367339b6920730a0fd4bfcd506aa78c4b6f237
Masibo <victor.wafula@student.moringaschool.com>
2023-07-17 13:35:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const event = document.addEventListener("DOMContentLoaded", function() {
+    const newText = document.getElementById("text");
+    newText.textContent = "This is really cool!"
+})
+
